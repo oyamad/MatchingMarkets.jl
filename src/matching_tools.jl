0e6d6c1daab949ba2ef4e08369bfce99dd3ce9aa
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-06-12 23:09:07 +0900

    Modify docstring

--- src/matching_tools.jl
+++ src/matching_tools.jl
@@ -8,6 +8,8 @@ Author: Daisuke Oyama
 # random_prefs
 
 """
+    random_prefs([rng, ]m, n, allow_unmatched=true)
+
 Generate random preference order lists for two groups, say, m males and n
 females.
 
@@ -15,9 +17,9 @@ Each male has a preference order over femals [1, ..., n] and "unmatched" which
 is represented by 0, while each female has a preference order over males
 [1, ..., m] and "unmatched" which is again represented by 0.
 
-The optional rng argument specifies a random number generator.
+The optional `rng` argument specifies a random number generator.
 
-##### Arguments
+# Arguments
 
 - `m::Integer` : Number of males.
 - `n::Integer` : Number of females.
@@ -25,14 +27,14 @@ The optional rng argument specifies a random number generator.
 males and females where 0 is always placed in the last place, (i.e.,
 "unmatched" is least preferred by every individual).
 
-##### Returns
+# Returns
 
 - `m_prefs::Matrix{Int}` :  Array of shape (n+1, m), where each column contains
 a random permutation of 0, 1, ..., n.
 - `f_prefs::Matrix{Int}` :  Array of shape (m+1, n), where each column contains
 a random permutation of 0, 1, ..., m.
 
-##### Examples
+# Examples
 
 ```julia
 julia> m_prefs, f_prefs = random_prefs(4, 3);
@@ -68,9 +70,7 @@ julia> f_prefs
  4  1  1
  3  4  2
  0  0  0
-
- ```
-
+```
 """
 function random_prefs(rng::AbstractRNG,
 	                  m::Integer, n::Integer; allow_unmatched::Bool=true)
@@ -119,7 +119,6 @@ end
 """
 Given a vector `a` of length n, generate a random permutation of length n and
 store it in `a`.
-
 """
 function _randperm!{T<:Integer}(r::AbstractRNG, a::AbstractVector{T})
     # a = Array{typeof(n)}(n)
@@ -145,7 +144,6 @@ end
 """
 Given an m x n matrix `a`, generate n random permutations of length m and store
 them in columns of `a`.
-
 """
 function _randperm2d!{T<:Integer}(r::AbstractRNG, a::AbstractMatrix{T})
     m, n = size(a)
